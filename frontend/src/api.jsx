00f2acb64ffb7d279a1d2f7e676ce5c000c7ba26
ankith26 <ankith26@users.noreply.github.com>
2024-12-05 12:40:37 +0000

    Apply Prettier Formatting Fixes

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -3,13 +3,13 @@ import axios from 'axios';
 const API_PREFIX = `${import.meta.env.VITE_BASE}api`;
 
 const api = axios.create({
-    baseURL: API_PREFIX,
-    withCredentials: true,
-    maxRedirects: 0,
+  baseURL: API_PREFIX,
+  withCredentials: true,
+  maxRedirects: 0,
 });
 
 function prefix_api(str) {
-    return API_PREFIX + str;
+  return API_PREFIX + str;
 }
 
 export { api, prefix_api };
